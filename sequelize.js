c1bcc848237054ded324ac8820012c58bc052926
manjunathbandaru <bandaru.manju@gmail.com>
2025-03-14 21:53:23 -0500

    Node update

--- sequelize.js
+++ sequelize.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("ecommerce_dbb", "user1", "password", {
-  host: "localhost",
-  dialect: "postgres",
+const sequelize = new Sequelize('graph', 'neondb_owner', 'npg_LP0bfzVa7hyW', {
+  host: 'ep-solitary-boat-a5thgwk3-pooler.us-east-2.aws.neon.tech',
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // For self-signed certificates if needed
+    },
+  },
 });
 
 sequelize
